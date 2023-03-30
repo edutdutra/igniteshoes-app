c2cdeae49d44a27b8fd358ba6294a5a2c4d93611
Eduardo-Dutra <edutdutra@gmail.com>
2023-03-30 19:30:18 -0300

    add pre build

--- App.tsx
+++ App.tsx
@@ -9,6 +9,10 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+import OneSignal from 'react-native-onesignal';
+
+OneSignal.setAppId("a8e0036c-4d82-4247-ad6f-944476a0ffcd");
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
