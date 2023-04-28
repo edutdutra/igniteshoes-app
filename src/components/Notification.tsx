b17ab7a13f8d009e9120c1ac5170bdfa691544b4
edutdutra <edutdutra@gmail.com>
2023-04-28 09:54:41 -0300

    add deep linking

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,29 +1,23 @@
 import {HStack, Text, IconButton, CloseIcon, Icon, Pressable} from 'native-base';
 import {Ionicons} from '@expo/vector-icons';
-import {useNavigation} from "@react-navigation/native";
 import {OSNotification} from "react-native-onesignal";
+import * as Linking from 'expo-linking';
 
 type Props = {
     data: OSNotification;
     onClose: () => void;
 }
 
-type AdditionalDataProps = {
-    route?: string;
-    product_id?: string;
-}
+
 
 export function Notification({data, onClose}: Props) {
-    const {navigate} = useNavigation();
 
     function handleOnPress() {
-        const {route, product_id} = data.additionalData as AdditionalDataProps;
-
-        if (route === 'details' && product_id) {
-            navigate(route, {productId: product_id})
+        if (data.launchURL) {
+            Linking.openURL(data.launchURL);
+            onClose();
         }
 
-        onClose();
     }
 
     return (
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,37 +1,52 @@
 import {useEffect, useState} from "react";
-import { useTheme } from 'native-base';
+import {useTheme} from 'native-base';
 import OneSignal, {OSNotification} from "react-native-onesignal";
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
 
-import { AppRoutes } from './app.routes';
+import {AppRoutes} from './app.routes';
 import {Notification} from "../components/Notification";
 
+const linking = {
+    prefixes: ['igniteshoesapp://', 'com.rocketseat.igniteshoes://', 'exp+igniteshoesapp://'],
+    config: {
+        screens: {
+            details: {
+                path: 'details/:productId',
+                parse: {
+                    productId: (productId: string) => productId
+                }
+            }
+        }
+    }
+}
+
+
 export function Routes() {
-  const [notification, setNotification] = useState<OSNotification>();
-  const { colors } = useTheme();
+    const [notification, setNotification] = useState<OSNotification>();
+    const {colors} = useTheme();
 
-  const theme = DefaultTheme;
-  theme.colors.background = colors.gray[700];
+    const theme = DefaultTheme;
+    theme.colors.background = colors.gray[700];
 
-  useEffect(() => {
-    OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent) => {
-      const response = notificationReceivedEvent.getNotification();
-      setNotification(response)
-    });
+    useEffect(() => {
+        OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent) => {
+            const response = notificationReceivedEvent.getNotification();
+            setNotification(response)
+        });
 
-  }, [])
+    }, [])
 
-  return (
-    <NavigationContainer theme={theme}>
-      <AppRoutes />
+    return (
+        <NavigationContainer theme={theme} linking={linking}>
+            <AppRoutes/>
 
-      {
-          notification?.title &&
-          <Notification
-              data={notification}
-              onClose={() => setNotification(undefined)}
-          />
-      }
-    </NavigationContainer>
-  );
+            {
+                notification?.title &&
+                <Notification
+                    data={notification}
+                    onClose={() => setNotification(undefined)}
+                />
+            }
+        </NavigationContainer>
+    );
 }
